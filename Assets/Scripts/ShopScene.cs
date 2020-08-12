73a23d5d553187425edd2080b556bc37e5e63c50
Bopanna <bopannaa@gmail.com>
2020-08-12 21:47:25 +0530

    Implemented UI for ShopScene
    added DotTween package
    added ui gun models
    
    added UiGuns script
    added ShopScene script
    
    added movement animations for guns
    
    added seperate stacking camera to base camera for rendering ui guns

---
+++ Assets/Scripts/ShopScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ShopScene : MonoBehaviour
+{
+    public static Action ActionBackButton;
+    public static Action ActionNextGunButton;
+    public static Action ActionPreviousGunButton;
+    
+    public  void OnBackButtonClick()
+    {
+        if(ActionBackButton != null)
+        {
+            ActionBackButton();
+        }
+    }
+    
+    public  void OnNextGunButtonClick()
+    {
+        if(ActionNextGunButton != null)
+        {
+            ActionNextGunButton();
+        }
+    }
+    
+    public  void OnPreviousGunButtonClick()
+    {
+        if(ActionPreviousGunButton != null)
+        {
+            ActionPreviousGunButton();
+        }
+    }
+}
---
+++ Assets/Scripts/UiGuns.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Linq;
+
+public class UiGuns : MonoBehaviour
+{
+    [SerializeField] private AnimationClip moveGunRightClip, moveGunLeftClip;
+    [SerializeField] private GameObject gunsParentX, gunsParentY;
+    [SerializeField] private Camera gunsCamera;
+
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private GameObject[] guns;
+
+    Queue<GameObject> gunsQueue = new Queue<GameObject>();
+
+    bool isMoveRight = true;
+    bool isMoveLeft = false;
+
+    void Awake()
+    {
+        foreach (GameObject g in guns)
+        {
+            gunsQueue.Enqueue(g);
+        }
+    }
+
+    void OnEnable()
+    {
+        ShopScene.ActionPreviousGunButton += OnPreviousGunAction;
+        ShopScene.ActionNextGunButton += OnNextGunAction;
+    }
+
+    void OnDisable()
+    {
+        ShopScene.ActionPreviousGunButton -= OnPreviousGunAction;
+        ShopScene.ActionNextGunButton -= OnNextGunAction;
+    }
+
+    void OnNextGunAction()
+    {
+        animator.Play(moveGunLeftClip.name);
+    }
+
+    void OnPreviousGunAction()
+    {
+        animator.Play(moveGunRightClip.name);
+    }
+
+    float rotationX, rotationY;
+
+    void Update()
+    {
+        rotationX = Input.GetAxis("Mouse X") * 2;
+        rotationY = Input.GetAxis("Mouse Y") * 2;
+
+        if (Input.GetMouseButton(0))
+        {
+            gunsParentX.transform.DOBlendableLocalRotateBy(Vector3.down * rotationX, 1f);
+            gunsParentY.transform.DOBlendableLocalRotateBy(Vector3.back * rotationY, 1f);
+        }
+    }
+
+    void DisableGunsCamera(int dir)
+    {
+        gunsCamera.enabled = false;
+        if (dir > 0)
+        {
+            ResetGuns(isMoveRight);
+        }
+        if (dir < 0)
+        {
+            ResetGuns(isMoveLeft);
+        }
+
+    }
+
+    void EnableGunsCamera()
+    {
+        gunsCamera.enabled = true;
+    }
+
+    void ResetGuns(bool direction)
+    {
+        gunsParentX.transform.localEulerAngles = Vector3.zero;
+        gunsParentY.transform.localEulerAngles = Vector3.zero;
+        var gun = gunsQueue.Dequeue();
+        gun.SetActive(false);
+        if (direction)
+        {
+            gunsQueue = new Queue<GameObject>(gunsQueue.Reverse());
+            isMoveRight = true;
+            isMoveLeft = false;
+        }
+        gunsQueue.Enqueue(gun);
+        gunsQueue.Peek().SetActive(true);
+    }
+}
