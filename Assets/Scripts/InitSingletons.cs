dd3b4aa0b5ced7e34660a5eebabcd31b45ba9a7e
Bopanna <bopannaa@gmail.com>
2020-08-13 12:22:02 +0530

    implemented Abstract Singleton class
    Created Scene_Manager class as singleton
    Created InitSingleton object with class in startscene to init all singletons

---
+++ Assets/Scripts/InitSingletons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitSingletons : MonoBehaviour
+{
+    Scene_Manager scene_Manager;
+
+    void Awake()
+    {
+        scene_Manager = Scene_Manager.Instance;    
+    }
+}
---
+++ Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Scene_Manager : Singleton<Scene_Manager>
+{   
+    public void OnEnable()
+    {
+        ShopScene.ActionBackButton  += LoadStartScene;
+        StartScene.ActionShopButton += LoadShopScene;
+        StartScene.ActionPlayButton += LoadLevelScene;
+        LevelScene.ActionBackButton += LoadStartScene;
+    }
+
+    void OnDisable()
+    {
+        ShopScene.ActionBackButton  -= LoadStartScene;
+        StartScene.ActionShopButton -= LoadShopScene;
+        StartScene.ActionPlayButton -= LoadLevelScene;
+        LevelScene.ActionBackButton -= LoadStartScene;
+    }
+
+    public void LoadShopScene()
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void LoadLevelScene()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+}
+
