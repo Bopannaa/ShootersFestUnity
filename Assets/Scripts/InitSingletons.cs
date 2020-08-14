64263854e40d4507623246436bf871baf7b3ea70
Bopanna <bopannaa@gmail.com>
2020-08-14 18:05:04 +0530

    Added WorldEnviornment to StartScene
    Added new Skybox to main camera
    Added a custom shader for tree

--- Assets/Scripts/InitSingletons.cs
+++ Assets/Scripts/InitSingletons.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class InitSingletons : MonoBehaviour
 {
+    public GameObject worldEnvoirnment;
+
     Scene_Manager scene_Manager;
 
     void Awake()
     {
         scene_Manager = Scene_Manager.Instance;    
+        worldEnvoirnment.transform.parent = scene_Manager.transform;
     }
 }
